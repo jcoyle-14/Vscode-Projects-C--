239b63938aefcebd689fe6093ce015dee690c37f
jcoyle-14 <joeycoylefarmington@gmail.com>
2026-02-27 15:44:40 -0600

    Fight function

--- Week 7/gameArenapt2.cpp
+++ Week 7/gameArenapt2.cpp
@@ -5,25 +5,28 @@ using namespace std;
 void PrintWelcome();//Declaring functions , Table of contents
 void PrintStats(int health, int attack);
 int choice();
+int GetValidChoice();
 int CalculateDamage(int attack, int defense);
 bool CriticalHit();
-int GetValidChoice();
+void DisplayMenu();
+void DisplayChoosePrompt();
+void DisplayDamage(int damage);
+void DisplayNotCritical();
+void DisplayFightMessage();
+void DisplayRestMessage();
+void DisplayQuitMessage();
+void ApplyRest(int& health);
+void DoFight(int attack, int defense);
+void DoRest(int& health);
+void DoQuit(int& running);
 
 int main(){
     int health = 1, attack = 10, path, defense =5;
-    
+    int running = 1; //1 keep going , 0 = stop...
     
     PrintWelcome();
-    PrintStats(health, attack); //when you call a function dont add data types
-    path = choice();
-    cout<<"Damage is: "<<CalculateDamage(attack,defense);
-    cout<<"Damage is: "<<CalculateDamage(23,14);
-    if(CriticalHit()){
-        cout<<"That was a critical hit!";
-    }
-    else{
-        cout<<"You are okay... for now";
-    }
+    
+    
 }
 
 void PrintWelcome(){
@@ -34,14 +37,35 @@ void PrintStats(int health, int attack){
     cout<<"Health: "<<health<<endl;
     cout<<"Attack: "<<attack<<endl;
 }
+void DisplayMenu(){
+    cout<<"\n1. Fight\n";
+    cout<<"2. Rest\n";
+    cout<<"3. Quit\n";
+}
+void DisplayChoosePrompt(){
+    cout<<"Choose your path: ";
+}
+void DisplayDamage(int damage){
+    cout<<"Damage is: "<<damage<<endl;
+}
+void DisplayCriticalHit(){
+    cout<<"That was a critical hit!";
+}
+void DisplayNotCritical(){
+    cout<<"You are okay... for now.";
+}
+void DisplayFightMessage(){
+    cout<<"\nYou Chose to FIGHT!\n";
+}
+void DisplayRestMessage(){
+    cout<<"\nYou Chose to REST.\n";
+}
+void DisplayQuitMessage(){
+    cout<<"\nYou Chose to QUIT. :(\n";
+}
 int choice(){
     int num;
-    cout<<"\n1. Fight\n";
-    cout<<"\n2. Rest\n";
-    cout<<"\n3. Quit\n";       
-    cout<<"Choose your path: ";
     cin>>num;
-
     return num;
 }
 int CalculateDamage(int attack, int defense){
@@ -72,4 +96,25 @@ int GetValidChoice(){
     }while(choice != 1 && choice != 2 && choice != 3);
 
     return choice;
+}
+
+void ApplyRest (int& health){
+    health += 2;
+}
+
+void DoFight(int attack, int defense){
+    DisplayFightMessage();
+
+    int dmg1 = CalculateDamage(attack, defense);
+    DisplayDamage(dmg1);
+
+    int dmg2 = CalculateDamage(attack, defense);
+    DisplayDamage(dmg2);
+
+    if(CriticalHit()){
+        DisplayCriticalHit();
+    }
+    else{
+        DisplayNotCritical();
+    }
 }
